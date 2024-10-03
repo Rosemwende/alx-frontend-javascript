60d58da022a2413fae3ccfc4ce52ad9f4e734c0d
Rosemwende <rosemuema82@gmail.com>
2024-10-03 11:11:58 +0000

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,9 @@
 function getResponseFromAPI() {
-return new promise((resolve, reject) => {
-setTimeout(() => {
-resolve({status: 200, body: 'photo-profile-1'});
-}, 1000);
-});
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ status: 200, body: 'photo-profile-1' });
+    }, 1000);
+  });
 }
 
 export default getResponseFromAPI;
