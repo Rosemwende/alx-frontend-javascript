709d8a3f81d75e6cf9266bb7925c7798f920387e
Rosemwende <rosemuema82@gmail.com>
2024-10-03 17:48:39 +0000

    10-car.js

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,18 @@
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const Constructor = this.constructor[Symbol.species] || this.constructor;
+    return new Constructor();
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+}
+
+export default Car;
