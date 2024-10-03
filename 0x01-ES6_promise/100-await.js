5c17caec2c20f8640b32065961de1bce6e996df5
Rosemwende <rosemuema82@gmail.com>
2024-10-03 12:13:42 +0000

    100-await.js

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,12 @@
+import uploadPhoto from './5-photo-reject';
+import createUser from './4-user-promise';
+
+export default async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto('profile.jpg');
+    const user = await createUser('Guillaume', 'Salva');
+    return { photo, user };
+  } catch (error) {
+    return { photo: null, user: null };
+  }
+}
