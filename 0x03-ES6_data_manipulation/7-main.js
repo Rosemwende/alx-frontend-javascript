26d6c01fbb571cd17dce7e904e013e2b6025515a
Rosemwende <rosemuema82@gmail.com>
2024-10-08 20:56:28 +0000

    7-main.js

--- 0x03-ES6_data_manipulation/7-main.js
+++ 0x03-ES6_data_manipulation/7-main.js
@@ -1,5 +1,6 @@
 import hasValuesFromArray from "./7-has_array_values";
 
+const result = (value) => set.has(value);
 console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1]));
 console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [10]));
 console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1, 10]));
