8c40067270bfd0cb0f6e6bef2243ac532ef63478
Rosemwende <rosemuema82@gmail.com>
2024-10-15 10:42:13 +0000

    main.ts

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,33 +1,42 @@
-interface Student {
-  firstName: string;
-  lastName: string;
-  age: number;
-  location: string;
-}
+(() => {
+  interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+  }
 
-const student1: Student = {
-  firstName: 'Rose',
-  lastName: 'Mwende',
-  age: 29,
-  location: 'Kenya',
-};
+  const student1: Student = {
+    firstName: 'Rose',
+    lastName: 'Mwende',
+    age: 29,
+    location: 'Kenya',
+  };
 
-const student2: Student = {
-  firstName: 'Jane',
-  lastName: 'Smith',
-  age: 22,
-  location: 'Ghana',
-};
+  const student2: Student = {
+    firstName: 'Jane',
+    lastName: 'Smith',
+    age: 22,
+    location: 'Ghana',
+  };
 
-const studentsList: Student[] = [student1, student2];
+  const studentsList: Student[] = [student1, student2];
 
-const table = document.createElement('table');
-studentsList.forEach((student) => {
-  const row = table.insertRow();
-  const cell1 = row.insertCell(0);
-  const cell2 = row.insertCell(1);
-  cell1.textContent = student.firstName;
-  cell2.textContent = student.location;
-});
+  const table = document.createElement('table');
 
-document.body.appendChild(table);
+  const headerRow = table.insertRow();
+  const header1 = headerRow.insertCell(0);
+  const header2 = headerRow.insertCell(1);
+  header1.textContent = 'First Name';
+  header2.textContent = 'Location';
+
+  studentsList.forEach((student) => {
+    const row = table.insertRow();
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    cell1.textContent = student.firstName;
+    cell2.textContent = student.location;
+  });
+
+  document.body.appendChild(table);
+})();
