5c963f900c56b3d677af2db97157743aea2a6587
Rosemwende <rosemuema82@gmail.com>
2024-10-15 16:43:51 +0000

    main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -8,15 +8,16 @@ interface Teacher {
 }
 
 const teacher3: Teacher = {
-  firstName: 'John',
+  firstName: 'Rose',
   fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
+  lastName: 'Muema',
+  location: 'Kenya',
   contract: false,
 };
 
 console.log(teacher3);
 
+// task_1/js/main.ts (continuing)
 interface Directors extends Teacher {
   numberOfReports: number;
 }
@@ -35,3 +36,29 @@ function printTeacher(firstName: string, lastName: string): string {
 	return '${firstName[0]} ${lastName}';
 }
 printTeacher("Rose", "Mwende");
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClass;
+}
+
+interface StudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class Student implements StudentClass {
+  constructor(public firstName: string, public lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new Student("Rose", "Mwende");
+console.log(student.displayName());
+console.log(student.workOnHomework());
+
