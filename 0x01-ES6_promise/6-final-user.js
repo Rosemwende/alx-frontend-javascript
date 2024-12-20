73539cf41ee12cf4314c286cefac74db092ec6ac
rosemwende <rosemuema82@gmail.com>
2024-12-20 19:37:29 +0000

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,12 +5,11 @@ function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
   const uploadPhotoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([signUpPromise, uploadPhotoPromise]).then(
-    (results) =>
-      results.map((result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
-      }))
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise]).then(results =>
+    results.map(result => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason
+    }))
   );
 }
 
