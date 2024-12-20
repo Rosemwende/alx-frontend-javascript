f1c8c94adf1571e510969a3375658de73d213d6c
rosemwende <rosemuema82@gmail.com>
2024-12-20 18:48:11 +0000

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,14 @@
 function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
     .catch(() => {
-      return new Error();
+      return new Error('Error');
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
     });
 }
 
