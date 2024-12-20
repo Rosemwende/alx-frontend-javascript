993ea2d111191fe89d4d8ad0ff5532b49689d8e3
rosemwende <rosemuema82@gmail.com>
2024-12-20 14:00:09 +0000

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,14 @@
 export default function cleanSet(set, startString) {
   if (typeof startString !== 'string') {
     return '';
-  }
 
   if (startString === '') {
     return [...set].join('-');
   }
+
   const result = [...set]
     .filter((item) => item.startsWith(startString))
-    .map((item) => item.slice(startString.length));
+    .map((item) => item.slice(startString.length)));
 
   return result.join('-');
 }
