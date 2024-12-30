f2a738359362b0161fb41fc06b76288bb7e51200
rosemwende <rosemuema82@gmail.com>
2024-12-30 14:37:10 +0000

    100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,15 +1,13 @@
-import Car from './10-car.js';
+import Car from './10-car';
 
 class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    // Call the parent constructor to initialize brand, motor, and color
     super(brand, motor, color);
-    this._range = range; // Initialize the range attribute
+    this._range = range;
   }
 
-  // Override the cloneCar method
   cloneCar() {
-    return new Car(); // Return an instance of Car instead of EVCar
+    return new Car();
   }
 }
 
